e713cb50a8fa0963cad2de3024c53a0ece10787b
Cass Enos <enosdoc@gmail.com>
2026-02-27 10:50:39 -0800

    created schema and db filez

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,15 @@
--- TODO
+DROP TABLE IF EXISTS folders CASCADE;
+DROP TABLE IF EXISTS files;
+
+CREATE TABLE folders (
+  id SERIAL PRIMARY KEY,
+  name TEXT UNIQUE NOT NULL
+);
+
+create table files (
+  id SERIAL PRIMARY KEY,
+  name text NOT NULL,
+  size integer NOT NULL,
+  folder_id integer NOT NULL REFERENCES folders(id) ON DELETE CASCADE,
+  UNIQUE (name, folder_id)
+);
